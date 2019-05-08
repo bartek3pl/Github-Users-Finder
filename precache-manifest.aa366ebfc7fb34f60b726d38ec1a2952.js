a5326b017a9fcf6a9d5e1c16eeb12418f5715488
bartek3pl <bartekh1998@op.pl>
2019-05-08 22:24:36 +0200

    Updates

---
+++ precache-manifest.aa366ebfc7fb34f60b726d38ec1a2952.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5896d3f9421a32ce36df82fafe55e25d",
+    "url": "/Github-User-Finder/static/media/user.5896d3f9.png"
+  },
+  {
+    "revision": "cf33ec77a7ca266733e9a0e9e8641db7",
+    "url": "/Github-User-Finder/static/media/star.cf33ec77.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/Github-User-Finder/static/media/logo.76d344f0.svg"
+  },
+  {
+    "revision": "e87899aec4a72f04c2466f49e6d40f8f",
+    "url": "/Github-User-Finder/static/media/like.e87899ae.svg"
+  },
+  {
+    "revision": "1a729de97e671bd612eed77cbe37cbf9",
+    "url": "/Github-User-Finder/static/media/friendship.1a729de9.svg"
+  },
+  {
+    "revision": "b050e75b159eff6a8ed0",
+    "url": "/Github-User-Finder/static/js/runtime~main.be004260.js"
+  },
+  {
+    "revision": "ce75b8713eaa89ba8ce8",
+    "url": "/Github-User-Finder/static/js/main.2f16e2a2.chunk.js"
+  },
+  {
+    "revision": "818ee7306939d03d477c",
+    "url": "/Github-User-Finder/static/js/2.95435f47.chunk.js"
+  },
+  {
+    "revision": "ce75b8713eaa89ba8ce8",
+    "url": "/Github-User-Finder/static/css/main.1277e47f.chunk.css"
+  },
+  {
+    "revision": "5901b94cefb77be3106308c0b1852882",
+    "url": "/Github-User-Finder/index.html"
+  }
+];
