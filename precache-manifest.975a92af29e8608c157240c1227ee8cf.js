5ab9ebf0bbbcf6c436fbe0f4b38c4082cb10cbf7
bartek3pl <bartekh1998@op.pl>
2019-10-17 13:37:00 +0200

    Updates

---
+++ precache-manifest.975a92af29e8608c157240c1227ee8cf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca3c1379896382e2629e",
+    "url": "/Github-User-Finder/static/css/main.34769431.chunk.css"
+  },
+  {
+    "revision": "ca3c1379896382e2629e",
+    "url": "/Github-User-Finder/static/js/main.2ec230fb.chunk.js"
+  },
+  {
+    "revision": "b050e75b159eff6a8ed0",
+    "url": "/Github-User-Finder/static/js/runtime~main.be004260.js"
+  },
+  {
+    "revision": "2a4a6e629923fb938c3c",
+    "url": "/Github-User-Finder/static/js/2.7673e99b.chunk.js"
+  },
+  {
+    "revision": "cf33ec77a7ca266733e9a0e9e8641db7",
+    "url": "/Github-User-Finder/static/media/star.cf33ec77.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/Github-User-Finder/static/media/logo.76d344f0.svg"
+  },
+  {
+    "revision": "1a729de97e671bd612eed77cbe37cbf9",
+    "url": "/Github-User-Finder/static/media/friendship.1a729de9.svg"
+  },
+  {
+    "revision": "e87899aec4a72f04c2466f49e6d40f8f",
+    "url": "/Github-User-Finder/static/media/like.e87899ae.svg"
+  },
+  {
+    "revision": "5896d3f9421a32ce36df82fafe55e25d",
+    "url": "/Github-User-Finder/static/media/user.5896d3f9.png"
+  },
+  {
+    "revision": "ba9458353508173a4b98df6553e4dceb",
+    "url": "/Github-User-Finder/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Github-User-Finder/precache-manifest.3e0b06c0122ddca22ad914b437c7fc5c.js"
+  "/Github-User-Finder/precache-manifest.975a92af29e8608c157240c1227ee8cf.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/Github-User-Finder/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
